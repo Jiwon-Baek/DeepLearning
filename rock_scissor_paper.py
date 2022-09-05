d7982782e880e256b605906baf6af23b6fbf4502
Jiwon Baek <104557198+Jiwon-Baek@users.noreply.github.com>
2022-09-06 00:22:11 +0900

    Add files via upload

---
+++ rock_scissor_paper.py
@@ -0,0 +1,62 @@
+import tensorflow as tf
+from tensorflow import keras
+
+model=keras.models.Sequential()
+model.add(keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(28,28,3)))
+model.add(keras.layers.MaxPool2D(2,2))
+model.add(keras.layers.Conv2D(32, (3,3), activation='relu'))
+model.add(keras.layers.MaxPooling2D((2,2)))
+model.add(keras.layers.Flatten())
+model.add(keras.layers.Dense(32, activation='relu'))
+model.add(keras.layers.Dense(3, activation='softmax'))
+
+print('Model에 추가된 Layer 개수: ', len(model.layers))
+
+model.summary()
+model.compile(optimizer='adam',
+             loss='sparse_categorical_crossentropy',
+             metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=20)
+def load_test(img_path, number_of_data=346):  # 가위바위보 이미지 개수 총합에 주의하세요.
+    # 가위 : 0, 바위 : 1, 보 : 2
+    img_size=28
+    color=3
+    #이미지 데이터와 라벨(가위 : 0, 바위 : 1, 보 : 2) 데이터를 담을 행렬(matrix) 영역 생성
+    imgs=np.zeros(number_of_data*img_size*img_size*color,dtype=np.int32).reshape(number_of_data,img_size,img_size,color)
+    labels=np.zeros(number_of_data,dtype=np.int32)
+
+    idx=0
+    for file in glob.iglob(img_path+'/scissor/*.jpg'):
+        img = np.array(Image.open(file),dtype=np.int32)
+        imgs[idx,:,:,:]=img    # 데이터 영역에 이미지 행렬을 복사
+        labels[idx]=0   # 가위 : 0
+        idx=idx+1
+
+    for file in glob.iglob(img_path+'/rock/*.jpg'):
+        img = np.array(Image.open(file),dtype=np.int32)
+        imgs[idx,:,:,:]=img    # 데이터 영역에 이미지 행렬을 복사
+        labels[idx]=1   # 바위 : 1
+        idx=idx+1       
+    
+    for file in glob.iglob(img_path+'/paper/*.jpg'):
+        img = np.array(Image.open(file),dtype=np.int32)
+        imgs[idx,:,:,:]=img    # 데이터 영역에 이미지 행렬을 복사
+        labels[idx]=2   # 보 : 2
+        idx=idx+1
+        
+    print("테스트데이터(x_test)의 이미지 개수는",idx,"입니다.")
+    return imgs, labels
+
+test_dir_path = os.getenv("HOME") + "/aiffel/rock_scissor_paper_data/"   # 테스트 데이터 경로 설정 
+
+resize_images(os.path.join(test_dir_path, "rock"))
+resize_images(os.path.join(test_dir_path, "scissor"))
+resize_images(os.path.join(test_dir_path, "paper"))
+
+(x_test, y_test)=load_test(test_dir_path)  # 테스트 데이터 불러오기 
+x_test_norm = x_test/255.0   # 입력은 0~1 사이의 값으로 정규화
+
+print("x_test shape: {}".format(x_test.shape))
+print("y_test shape: {}".format(y_test.shape))
+model.evaluate(x_test_norm, y_test)
